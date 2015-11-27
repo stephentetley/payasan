1235a70c132dd23ef3006c689e82be8c248f49e0
stephentetley <stephen.tetley@gmail.com>
2015-11-27 19:43:56 +0000

    Ported Metrics and Transform from Monophonic syntax to Cadenza syntax.

---
+++ src/Payasan/LilyPond/Cadenza/Internal/Metrics.hs
@@ -0,0 +1,146 @@
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# OPTIONS -Wall #-}
+
+--------------------------------------------------------------------------------
+-- |
+-- Module      :  Payasan.LilyPond.Cadenza.Internal.Metrics
+-- Copyright   :  (c) Stephen Tetley 2015
+-- License     :  BSD3
+--
+-- Maintainer  :  stephen.tetley@gmail.com
+-- Stability   :  unstable
+-- Portability :  GHC
+--
+-- Metrics  - barcount, lowest pitch, highest pitch...
+--
+-- Contours.
+--
+--------------------------------------------------------------------------------
+
+module Payasan.LilyPond.Cadenza.Internal.Metrics
+  (
+
+    lowestPitch
+  , highestPitch
+
+  , lowestStep
+  , highestStep
+
+  , semitoneInterval
+
+  , grossContour
+
+  , refinedContour
+
+
+  ) where
+
+
+import Payasan.LilyPond.Cadenza.Internal.Syntax
+import Payasan.LilyPond.Cadenza.Internal.Traversals
+
+import Payasan.Base.Internal.Base
+import Payasan.Base.Internal.Contour
+import Payasan.Base.Internal.RewriteMonad
+
+import Payasan.Base.Pitch
+import Payasan.Base.ScaleDegree
+
+
+
+
+-- Simple metrics
+
+
+lowestPitch :: Phrase Pitch drn anno -> Maybe Pitch
+lowestPitch = foldPitch fn Nothing
+  where
+    fn Nothing   p                      = Just p
+    fn (Just p0) p | p `isLower` p0     = Just p
+                   | otherwise          = Just p0
+
+highestPitch :: Phrase Pitch drn anno -> Maybe Pitch
+highestPitch = foldPitch fn Nothing
+  where
+    fn Nothing   p                      = Just p
+    fn (Just p0) p | p `isHigher` p0    = Just p
+                   | otherwise          = Just p0
+
+
+lowestStep :: Phrase ChromaticPitch drn anno -> Maybe DiatonicPitch
+lowestStep = fmap diatonic_base . foldPitch fn Nothing
+  where
+    fn Nothing   s                      = Just s
+    fn (Just s0) s | s `isLower` s0     = Just s
+                   | otherwise          = Just s0
+
+
+highestStep :: Phrase ChromaticPitch drn anno -> Maybe DiatonicPitch
+highestStep = fmap diatonic_base . foldPitch fn Nothing
+  where
+    fn Nothing   s                      = Just s
+    fn (Just s0) s | s `isHigher` s0    = Just s
+                   | otherwise          = Just s0
+
+
+
+--------------------------------------------------------------------------------
+-- Contours
+
+
+contourAlgo :: (Pitch -> Pitch -> ctour) 
+            -> MonoPitchAlgo (Maybe Pitch) Pitch ctour
+contourAlgo comp = MonoPitchAlgo { initial_stateP = Nothing
+                                 , element_trafoP = fn }
+  where   
+    fn (Note p d a t)   = do { opt <- get 
+                             ; case opt of 
+                                  Nothing -> put (Just p) >> return (Rest d)
+                                  Just p0 -> 
+                                     let ct = comp p0 p
+                                     in put (Just p) >> return (Note ct d a t)
+                             }
+
+    fn (Rest d)         = pure $ Rest d
+    fn (Spacer d)       = pure $ Spacer d
+    fn (Skip d)         = pure $ Skip d
+    fn (Punctuation s)  = pure $ Punctuation s
+
+
+semitoneInterval :: forall drn anno. 
+                    Phrase Pitch drn anno -> Phrase Int drn anno
+semitoneInterval = transformP (contourAlgo comp)
+  where
+    comp pold pnew = let sc = interval_semitones $ intervalBetween pold pnew
+                     in if pnew `isLower` pold then negate sc else sc
+
+
+
+
+
+grossContour :: forall drn anno. 
+                Phrase Pitch drn anno -> Phrase GrossContour drn anno
+grossContour = transformP (contourAlgo comp)
+  where
+    comp pold pnew | pnew `isHigher` pold = UP
+                   | pnew `isLower`  pold = DOWN
+                   | otherwise            = GROSS_SAME
+
+
+
+
+
+refinedContour :: forall drn anno. 
+                  Phrase Pitch drn anno -> Phrase RefinedContour drn anno
+refinedContour = transformP (contourAlgo comp)
+  where
+    comp pold pnew 
+        | pnew `isHigher` pold = let ival = intervalBetween pold pnew
+                                 in if interval_distance ival > 2 
+                                    then LEAP_UP else STEP_UP
+
+        | pnew `isLower`  pold = let ival = intervalBetween pnew pold
+                                 in if interval_distance ival > 2 
+                                    then LEAP_DOWN else STEP_DOWN
+
+        | otherwise            = REFINED_SAME
---
+++ src/Payasan/LilyPond/Cadenza/Internal/Transform.hs
@@ -0,0 +1,159 @@
+{-# OPTIONS -Wall #-}
+
+--------------------------------------------------------------------------------
+-- |
+-- Module      :  Payasan.LilyPond.Cadenza.Internal.Transform
+-- Copyright   :  (c) Stephen Tetley 2015
+-- License     :  BSD3
+--
+-- Maintainer  :  stephen.tetley@gmail.com
+-- Stability   :  unstable
+-- Portability :  GHC
+--
+-- Musical transfomations (augmentation, diminution etc.)
+--
+--------------------------------------------------------------------------------
+
+module Payasan.LilyPond.Cadenza.Internal.Transform
+  (
+    augment
+  , diminute
+
+  , transposeChromatic
+  , transposeDiatonic
+
+  , retrograde
+  , invertChromatic
+  , invertDiatonic
+
+  , diatonicsFromTop -- temp
+
+  ) where
+
+
+import Payasan.LilyPond.Cadenza.Internal.Metrics
+import Payasan.LilyPond.Cadenza.Internal.Syntax
+import Payasan.LilyPond.Cadenza.Internal.Traversals
+
+import Payasan.Base.Internal.CommonSyntax
+import Payasan.Base.Internal.RewriteMonad
+
+import Payasan.Base.Duration
+import Payasan.Base.Names.DiatonicInterval
+import Payasan.Base.Names.Interval
+import Payasan.Base.Pitch
+import Payasan.Base.ScaleDegree
+
+
+-- | Double note lengths.
+--
+augment :: Phrase pch Duration anno -> Phrase pch Duration anno
+augment = mapDuration doubleDuration
+
+-- | Halve note lengths.
+--
+diminute :: Phrase pch Duration anno -> Phrase pch Duration anno
+diminute = mapDuration halveDuration
+
+
+-- | Transpose by an exact interval - this may produce 
+-- non-scale tones.
+--
+transposeChromatic :: Interval 
+                   -> Phrase Pitch drn anno 
+                   -> Phrase Pitch drn anno
+transposeChromatic ivl = mapPitch (.+^ ivl)
+
+
+
+
+addDiatonicIntervalC :: ChromaticPitch -> DiatonicInterval -> ChromaticPitch
+addDiatonicIntervalC (ChromaticPitch dp a) ivl = 
+    ChromaticPitch (dp `addDiatonicInterval` ivl) a
+
+
+
+transposeDiatonic :: DiatonicInterval 
+                  -> Phrase Pitch drn anno 
+                  -> Phrase Pitch drn anno
+transposeDiatonic ivl ph = interScaleStep (mapPitch (`addDiatonicIntervalC` ivl)) ph
+
+
+
+retrograde :: Phrase pch Duration anno -> Phrase pch Duration anno
+retrograde (Phrase info gs) = Phrase info $ map revNG $ reverse gs
+  where
+    revNG (Atom e)          = Atom e
+    revNG (Beamed es)       = Beamed $ map revNG $ reverse es
+    revNG (Tuplet spec es)  = Tuplet spec $ map revNG $ reverse es
+
+
+-- | Note - seems to need /scale degrees/ - taking interal with 
+-- top note and adding same interval to lowest note does not work.
+--
+invertChromatic :: Phrase Pitch drn anno -> Phrase Pitch drn anno
+invertChromatic ph = case lowestPitch ph of 
+    Nothing -> ph
+    Just p0 -> mapPitch (\ival -> p0 .+^ ival) $ intervalsFromTop ph
+
+
+intervalsFromTop :: Phrase Pitch drn anno -> Phrase Interval drn anno
+intervalsFromTop ph = case highestPitch ph of
+    Nothing -> mapPitch (const perfect_unison) ph         -- notelist is empty or just rests
+    Just top -> mapPitch (\p -> p `intervalBetween` top) ph 
+
+
+-- | 08 Oct - this is now wrong due to changes to ScaleDegree!
+--
+invertDiatonic :: Phrase Pitch drn anno -> Phrase Pitch drn anno
+invertDiatonic = interScaleStep invertDiatonic1
+
+invertDiatonic1 :: Phrase ChromaticPitch drn anno -> Phrase ChromaticPitch drn anno
+invertDiatonic1 ph = case lowestStep ph of 
+    Nothing -> ph
+    Just p0 -> mapPitch (\ival -> ChromaticPitch (p0 `addDiatonicInterval` ival) 0) $ diatonicsFromTop ph
+
+
+diatonicsFromTop :: Phrase ChromaticPitch drn anno -> Phrase DiatonicInterval drn anno
+diatonicsFromTop ph = case highestStep ph of
+    Nothing -> mapPitch (const simple_unison) ph         -- notelist is empty or just rests
+    Just top -> mapPitch (\p -> diatonic_base p `diatonicIntervalBetween` top) ph 
+
+
+
+interScaleStep :: (Phrase ChromaticPitch drn anno -> Phrase ChromaticPitch drn anno)
+               -> Phrase Pitch drn anno
+               -> Phrase Pitch drn anno
+interScaleStep fn = fromScaleStepRepr . fn . toScaleStepRepr
+
+toScaleStepRepr :: Phrase Pitch drn anno -> Phrase ChromaticPitch drn anno
+toScaleStepRepr = transformP step_algo
+  where
+    step_algo = MonoPitchAlgo { initial_stateP = ()
+                              , element_trafoP = change
+                              }
+
+    change (Note p d a t)       = (\p1 -> Note p1 d a t) <$> mf p
+    change (Rest d)             = pure $ Rest d
+    change (Spacer d)           = pure $ Spacer d
+    change (Skip d)             = pure $ Skip d
+    change (Punctuation s)      = pure $ Punctuation s
+
+    mf pch = (\key -> toChromaticPitch key pch) <$> asks local_key
+
+
+fromScaleStepRepr :: Phrase ChromaticPitch drn anno -> Phrase Pitch drn anno
+fromScaleStepRepr = transformP step_algo
+  where
+    step_algo = MonoPitchAlgo { initial_stateP = ()
+                              , element_trafoP = change
+                              }
+
+    change (Note p d a t)       = (\p1 -> Note p1 d a t) <$> mf p
+    change (Rest d)             = pure $ Rest d
+    change (Spacer d)           = pure $ Spacer d
+    change (Skip d)             = pure $ Skip d
+    change (Punctuation s)      = pure $ Punctuation s
+
+    mf oss = (\key -> fromChromaticPitch key oss) <$> asks local_key
+
